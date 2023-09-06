36e08cfc849c5f8b6b58a2089f3ce9fd01f3c7e5
ryanmartinez24 <ryanmartinez1024@gmai.com>
2023-09-06 15:12:30 -0400

    Add tests file and tested converting to quaternary

---
+++ src/main/java/Tests/Tests.java
@@ -0,0 +1,14 @@
+package Tests;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class Tests {
+    @Test
+    public void sanityTest() {
+        assertEquals(1, 1);
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module QuaternaryCalculator {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires org.junit.jupiter.api;
+    requires junit;
 
     opens QuaternaryCalculator to javafx.fxml;
     exports QuaternaryCalculator;
