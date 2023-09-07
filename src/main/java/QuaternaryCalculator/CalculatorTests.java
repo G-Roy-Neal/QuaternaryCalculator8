216d9af7928971fcd89c6165ea9eb4c81f3cd620
ryanmartinez24 <ryanmartinez1024@gmai.com>
2023-09-07 16:06:40 -0400

    Test mult, div, and start testing sqrt

--- src/main/java/QuaternaryCalculator/CalculatorTests.java
+++ src/main/java/QuaternaryCalculator/CalculatorTests.java
@@ -45,13 +45,13 @@ public class CalculatorTests {
     public void testAdd() {
         assertEquals(Integer.valueOf(22), Calculator.add(11,11));
         assertEquals(Integer.valueOf(-1), Calculator.add(11,-12));
-        //assertEquals(Integer.valueOf(0), Calculator.add(11, -11));
+        assertEquals(Integer.valueOf(0), Calculator.add(11, -11));
         assertEquals(Integer.valueOf(110), Calculator.add(22,22));
         assertEquals(Integer.valueOf(13), Calculator.add(3, 10));
     }
     @Test
     public void testSubtract() {
-        //assertEquals(Integer.valueOf(0), Calculator.sub(11, 11));
+        assertEquals(Integer.valueOf(0), Calculator.sub(11, 11));
         assertEquals(Integer.valueOf(1), Calculator.sub(12, 11));
         assertEquals(Integer.valueOf(-1), Calculator.sub(11, 12));
         assertEquals(Integer.valueOf(1012), Calculator.sub(1210, 132));
@@ -59,5 +59,27 @@ public class CalculatorTests {
         assertEquals(Integer.valueOf(-1203), Calculator.sub(1, 1210));
     }
 
+    @Test
+    public void testMult() {
+        assertEquals(Integer.valueOf(0), Calculator.mult(0, 12));
+        assertEquals(Integer.valueOf(121), Calculator.mult(11, 11));
+        assertEquals(Integer.valueOf(30), Calculator.mult(3, 10));
+        assertEquals(Integer.valueOf(1), Calculator.mult(-1, -1));
+        assertEquals(Integer.valueOf(-1), Calculator.mult(-1, 1));
+        assertEquals(Integer.valueOf(33220), Calculator.mult(1210, 22));
+    }
+
+    @Test
+    public void testDiv() {
+        assertEquals(Integer.valueOf(11), Calculator.div(121, 11));
+        assertEquals(Integer.valueOf(10), Calculator.div(30, 3));
+        assertEquals(Integer.valueOf(20), Calculator.div(200, 10));
+        assertEquals(Integer.valueOf(22), Calculator.div(1210, 22));
+    }
+
+    @Test
+    public void testSqrt() {
+        assertEquals(Integer.valueOf(2), Calculator.sqroot(11));
+    }
 
 }
